96fa0e45a311ac6dacf5b687e7428b1d15cd82cf
ClebersonT <39893338+ClebersonT@users.noreply.github.com>
2018-07-17 10:49:32 -0400

    Add files via upload

---
+++ Veiculo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package locadora;
+
+/**
+ *
+ * @author SUPORTE
+ */
+public class Veiculo {
+    
+    public int placa;
+    public String cor;
+    public String fabricante;
+    public float valorDiaria;
+    
+}
